10378a8e74b381d403738b9053bc6e865fa356e0
Gustavo Lobo Aguilar <tavo.lobo@gmail.com>
2026-01-30 14:57:54 -0600

    Update news_digest.py

--- news_digest.py
+++ news_digest.py
@@ -11,7 +11,7 @@ import pytz
 
 
 CONFIG = {
-    'notion_token': os.environ.get('DIGEST_TOKEN'), 
+    'notion_token': os.environ.get('NOTION_TOKEN'), 
     'database_id': '5ad52157ba4d490aa5b8364a0fa56ca3',
     'parent_page_id': '2f79f7560dbc802a846dc2f52fd4a26a',  # Extract from home page
     'timezone': 'America/Guatemala',
